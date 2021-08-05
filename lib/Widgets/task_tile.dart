8f0926c0cf55fceac93d163b5312c994c759a199
praneetk77 <praneetkarna77@gmail.com>
2021-08-05 16:21:06 +0545

    Lift checkbox state to task_lists

--- lib/Widgets/task_tile.dart
+++ lib/Widgets/task_tile.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function checkBoxCallback;
+
+  TaskTile({this.isChecked, this.taskTitle, this.checkBoxCallback});
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text("This is a task"),
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
       trailing: Checkbox(
-        value: false,
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: checkBoxCallback,
       ),
     );
   }
--- lib/Widgets/tasks_list.dart
+++ lib/Widgets/tasks_list.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:todoey_flutter/Widgets/task_tile.dart';
+import 'package:todoey_flutter/Models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: "Buy milk"),
+    Task(name: "Buy eggs"),
+    Task(name: "Buy bacon"),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [TaskTile(), TaskTile(), TaskTile()],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+            taskTitle: tasks[index].name,
+            isChecked: tasks[index].isDone,
+            checkBoxCallback: (bool checkBoxState) {
+              setState(() {
+                tasks[index].toggleDone();
+              });
+            });
+      },
+      itemCount: tasks.length,
     );
   }
 }
